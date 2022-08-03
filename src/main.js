0ee26e36e2d5f91f4be92e4546ebb796165fb70b
Andrelise28 <andrelisebuttner@hotmail.com>
2022-08-02 21:45:38 -0300

    Criação de site template de um site responsivo, estudo de html e css.

---
+++ src/main.js
@@ -0,0 +1,34 @@
+let contador = selector => {
+  $(selector).each(function () {
+    $(this)
+      .animate({
+        Counter: $(this).text()
+      }, {
+        duration: 4000,
+        easing: "swing",
+        step: function (value) {
+          $(this).text(Math.ceil(value));
+        }
+      });
+  });
+};
+
+let a = 0;
+$(window).scroll(function () {
+  let oTop = $(".numeros").offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() >= oTop) {
+    a++;
+    contador(".clientes > h1");
+  }
+});
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+  let oTop = $(".section-2").offset().top - window.innerHeight;
+  if ($(window).scrollTop() > oTop) {
+    navbar.addClass("sticky");
+  } else {
+    navbar.removeClass("sticky");
+  }
+});
